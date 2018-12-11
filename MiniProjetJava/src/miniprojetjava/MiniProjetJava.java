7ce1194114df55670b6ade7feb256e590c2dda67
Etienne Fonteix <etienne.fonteix@gmail.com>
2018-12-11 10:55:15 +0100

    Ajout Etape 1

--- MiniProjetJava/src/miniprojetjava/MiniProjetJava.java
+++ MiniProjetJava/src/miniprojetjava/MiniProjetJava.java
@@ -1,17 +1,22 @@
 package miniprojetjava;
 
+import java.io.IOException;
 import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 import miniprojetjava.film;
 
 public class MiniProjetJava {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         
         Scanner scanner = new Scanner( System.in );
-        int choix;
-        ArrayList liste = new ArrayList();
+        int choix, cpt;
+        String nomFilm, support = "", actPrinc, annee, realisateur, description;
+        String filmselect;
+        List<film> liste = new ArrayList<film>();
         
+        do {
         System.out.println("Saisir une option : ");
         System.out.println("Choix 0 : Fin du programme");
         System.out.println("Choix 1 : Ajouter un film");
@@ -21,63 +26,110 @@ public class MiniProjetJava {
         System.out.println("Choix 5 : Supprimer un film par le nom");
         System.out.println("Choix 6 : Afficher tous les films");
         choix = scanner.nextInt();
+        scanner.nextLine();
         
-        do {
             switch (choix) {
                     case 0 : //fin du prog
-
                         break;
+                        
                     case 1 : //Ajouter un film
-                        System.out.println("Saisir nom film : ");
-                        String nomFilm = scanner.nextLine();
+                        do {
+                            System.out.println("Saisir nom film : ");
+                            nomFilm = scanner.nextLine();
+                        } while (nomFilm.equals(""));
                         
                         System.out.println("Saisir acteur principal film : ");
-                        String actPrinc = scanner.nextLine();
+                        actPrinc = scanner.nextLine();
+                        
+                        System.out.println("Saisir année : ");
+                        annee = scanner.nextLine();
                         
                         System.out.println("Saisir realisateur film : ");
-                        String realisateur = scanner.nextLine();
+                        realisateur = scanner.nextLine();
                         
-                        System.out.println("Saisir support film (K7, USB, blue-ray, DVD) : ");
-                        String support = scanner.nextLine();
+                        System.out.println("Saisir description film : ");
+                        description = scanner.nextLine();
                         
-                        liste.add(new film(nomFilm, true, actPrinc, 1926, realisateur, support));
+                        do {
+                            System.out.println("Saisir support film (K7, USB, blue-ray, DVD) : ");
+                            support = scanner.nextLine();
+                        } while (!support.equals("K7") && !support.equals("USB") && !support.equals("blue-ray") && !support.equals("DVD"));
                         
+                        liste.add(new film(nomFilm, true, actPrinc, annee, realisateur, support, description));
                         break;
-                    case 2:
-                        //recherche un film
-                        break;
-                    case 3 : 
-                        //afficher film dispo
-                        break;
-                    case 4 : 
-                        //Afficher film par supp
-                        break;
-                    case 5 :
-                        //supp film
-                        break;
-                    case 6 : //afficher tous les films
-                        for(int i = 0; i < liste.size(); i++)
-                        {
-                          System.out.println(liste.get(i));
+                        
+                        
+                    case 2: //recherche un film
+                        System.out.println("Saisir nom film : ");
+                        nomFilm = scanner.nextLine();
+                        
+                        for (film filmListe : liste) {                            
+                            if (filmListe.GetNom().equals(nomFilm)) {
+                                filmselect=filmListe.FilmToString();
+                                System.out.println(filmselect);
+                            }
                         }
+                        System.out.println("\nAppuyez sur une touche pour continuer");
+                        System.in.read();
+                        break;
+                        
+                        
+                    case 3 : //afficher tous les film dispo
+                        for (film filmListe : liste) {
+                            if (filmListe.GetDispo() == true) {
+                                System.out.println(filmListe.GetNom());
+                            }
+                        }
+                        System.out.println("\nAppuyez sur une touche pour continuer");
+                        System.in.read();
+                        break;
+                        
+                        
+                    case 4 : //Afficher film par supp
+                        do {
+                            System.out.println("Saisir support film (K7, USB, blue-ray, DVD) : ");
+                            support = scanner.nextLine();
+                        } while (!support.equals("K7") && !support.equals("USB") && !support.equals("blue-ray") && !support.equals("DVD"));
+                        
+                        for (film filmListe : liste) {
+                            if (filmListe.GetSupp().equals(support)) {
+                                System.out.println(filmListe.GetNom());
+                            }
+                        }
+                        System.out.println("\nAppuyez sur une touche pour continuer");
+                        System.in.read();
+                        break;
+                        
+                        
+                    case 5 : //supprimer film
+                        System.out.println("Saisir nom film : ");
+                        nomFilm = scanner.nextLine();
+                        
+                        cpt = 0;
+                        for (film filmListe : liste) {
+                            //cpt++;
+                            if (filmListe.GetNom().equals(nomFilm)) {
+                                //liste.remove(cpt);
+                                liste.remove(filmListe);
+                            }
+                        }
+                        System.out.println("\nAppuyez sur une touche pour continuer");
+                        System.in.read();
+                        break;
+                        
+                        
+                    case 6 : //afficher tous les films
+                        for (film filmListe : liste) {
+                            filmselect = filmListe.FilmToString();
+                            System.out.println(filmselect);
+                        }
+                        System.out.println("\nAppuyez sur une touche pour continuer");
+                        System.in.read();
+                        break;
                     default:
                         System.out.println("Erreur de saisie");                        
+                        break;
             }
-        } while(choix != 0);
-        
-        /*
-        liste.add(12);
-        liste.add("Une chaîne de caractères !");
-        liste.add(12.20f);
-        liste.add('d');
-        
-        for(int i = 0; i < al.size(); i++)
-        {
-          System.out.println("donnée à l'indice " + i + " = " + al.get(i));
-        }
-*/
-        
-        
-    }
-    
+        } while(choix != 0);   
+    }    
 }
--- MiniProjetJava/src/miniprojetjava/film.java
+++ MiniProjetJava/src/miniprojetjava/film.java
@@ -5,17 +5,69 @@ public class film {
     public String nom;
     public boolean dispo;
     public String acteur;
-    public int annee;
+    public String annee;
     public String realisateur;
     public String support;
+    public String descriptif;
     
-    public film (String nom, boolean dispo, String acteur, int annee, String realisateur, String support) {
+    public String disponible;
+
+    public film (String nom, boolean dispo, String acteur, String annee, String realisateur, String support, String descriptif) {
         this.nom = nom;
         this.dispo = dispo;
         this.acteur = acteur;
         this.annee = annee;
         this.realisateur = realisateur;
-        this.support = support;   
+        this.support = support;
+        this.descriptif = descriptif;
+    }
+
+    public String GetNom(){return nom;}
     
+    public boolean GetDispo(){return dispo;}
+    
+    public String GetActeur(){return acteur;}
+
+    public String GetAnnee(){return annee;}
+
+    public String GetReal(){return realisateur;}
+
+    public String GetSupp(){return support;}
+
+    public String GetDesc(){return descriptif;}
+
+
+    public void SetNom(String nom){this.nom = nom;}
+
+    public void SetDispoTrue(){this.dispo = true;}
+
+    public void SetDispoFalse(){this.dispo = false;}
+
+    public void SetActeur(String acteur){this.acteur = acteur;}
+
+    public void SetAnnee(String annee){this.annee = annee;}
+
+    public void SetReal(String realisateur){this.realisateur = realisateur;}
+
+    public void SetSupp(String support){this.support = support;}
+
+    public void SetDesc(String descriptif){this.descriptif = descriptif;}
+
+    public String FilmToString() {
+        if(dispo == true) {
+            disponible = "Oui";
+        }
+        else {
+            disponible = "Non";
+        }
+    	return "\nNom du film : " + nom + "\nActeur : " + acteur + "\nAnnée : " + annee + "\nRéalisateur : " + realisateur + "\nSupport : " + support + "\nDescription : " + descriptif + "\nDisponible : " + disponible;
+    }
+    public void getDispoString() {
+        if(dispo == true) {
+            disponible = "Disponible";
+        }
+        else {
+            disponible = "Pas disponible";
+        }
     }
 }
