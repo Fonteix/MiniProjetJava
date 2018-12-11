59d095541c01c0edd62a06b6eeea1e55c11a710b
Etienne Fonteix <etienne.fonteix@gmail.com>
2018-12-11 11:45:54 +0100

    Final

--- MiniProjetJava/src/miniprojetjava/MiniProjetJava.java
+++ MiniProjetJava/src/miniprojetjava/MiniProjetJava.java
@@ -12,21 +12,37 @@ public class MiniProjetJava {
         
         Scanner scanner = new Scanner( System.in );
         int choix, cpt;
-        String nomFilm, support = "", actPrinc, annee, realisateur, description;
-        String filmselect;
-        List<film> liste = new ArrayList<film>();
+        String nomFilm, support = "", actPrinc, annee, realisateur, description, filmselect, nomClient, prenomClient, mailClient, clientSelect;
+        boolean clientExist;
+        
+        List<film> listeFilm = new ArrayList<film>();
+        List<client> listeClient = new ArrayList<client>();
+        
+        
+        //Liste de film exemple pour ne pas avoir à re-remplir à chaque test
+        listeFilm.add(new film("Avatar", true, "Brad", "2000", "Cameron", "USB", ""));
+        listeFilm.add(new film("Titanic", true, "Depp", "2010", "Speilbarg", "USB", ""));
+        listeFilm.add(new film("Taxi", true, "Danny", "2012", "Eastwood", "K7", ""));
+        
+        listeClient.add(new client("Dupond", "Jean", "jean.deupond@mail.com"));
+        listeClient.add(new client("Jaquet", "Pierre", "Maurice.Jaquet@mail.com"));
+        
         
         do {
-        System.out.println("Saisir une option : ");
-        System.out.println("Choix 0 : Fin du programme");
-        System.out.println("Choix 1 : Ajouter un film");
-        System.out.println("Choix 2 : Rechercher un film");
-        System.out.println("Choix 3 : Afficher les films disponibles");
-        System.out.println("Choix 4 : Afficher les films par support");
-        System.out.println("Choix 5 : Supprimer un film par le nom");
-        System.out.println("Choix 6 : Afficher tous les films");
-        choix = scanner.nextInt();
-        scanner.nextLine();
+            System.out.println("Saisir une option : ");
+            System.out.println("Choix 0 : Fin du programme");
+            System.out.println("Choix 1 : Ajouter un film");
+            System.out.println("Choix 2 : Rechercher un film");
+            System.out.println("Choix 3 : Afficher les films disponibles");
+            System.out.println("Choix 4 : Afficher les films par support");
+            System.out.println("Choix 5 : Supprimer un film par le nom");
+            System.out.println("Choix 6 : Afficher tous les films");
+            System.out.println("Choix 7 : Ajouter client");
+            System.out.println("Choix 8 : Afficher liste de client");
+            System.out.println("Choix 9 : Ajouter film à un client");
+            System.out.println("Choix 10 : Afficher tous les films d'un client");
+            choix = scanner.nextInt();
+            scanner.nextLine();
         
             switch (choix) {
                     case 0 : //fin du prog
@@ -55,7 +71,7 @@ public class MiniProjetJava {
                             support = scanner.nextLine();
                         } while (!support.equals("K7") && !support.equals("USB") && !support.equals("blue-ray") && !support.equals("DVD"));
                         
-                        liste.add(new film(nomFilm, true, actPrinc, annee, realisateur, support, description));
+                        listeFilm.add(new film(nomFilm, true, actPrinc, annee, realisateur, support, description));
                         break;
                         
                         
@@ -63,7 +79,7 @@ public class MiniProjetJava {
                         System.out.println("Saisir nom film : ");
                         nomFilm = scanner.nextLine();
                         
-                        for (film filmListe : liste) {                            
+                        for (film filmListe : listeFilm) {                            
                             if (filmListe.GetNom().equals(nomFilm)) {
                                 filmselect=filmListe.FilmToString();
                                 System.out.println(filmselect);
@@ -75,7 +91,7 @@ public class MiniProjetJava {
                         
                         
                     case 3 : //afficher tous les film dispo
-                        for (film filmListe : liste) {
+                        for (film filmListe : listeFilm) {
                             if (filmListe.GetDispo() == true) {
                                 System.out.println(filmListe.GetNom());
                             }
@@ -91,7 +107,7 @@ public class MiniProjetJava {
                             support = scanner.nextLine();
                         } while (!support.equals("K7") && !support.equals("USB") && !support.equals("blue-ray") && !support.equals("DVD"));
                         
-                        for (film filmListe : liste) {
+                        for (film filmListe : listeFilm) {
                             if (filmListe.GetSupp().equals(support)) {
                                 System.out.println(filmListe.GetNom());
                             }
@@ -106,11 +122,9 @@ public class MiniProjetJava {
                         nomFilm = scanner.nextLine();
                         
                         cpt = 0;
-                        for (film filmListe : liste) {
-                            //cpt++;
+                        for (film filmListe : listeFilm) {
                             if (filmListe.GetNom().equals(nomFilm)) {
-                                //liste.remove(cpt);
-                                liste.remove(filmListe);
+                                listeFilm.remove(filmListe);
                             }
                         }
                         System.out.println("\nAppuyez sur une touche pour continuer");
@@ -119,13 +133,75 @@ public class MiniProjetJava {
                         
                         
                     case 6 : //afficher tous les films
-                        for (film filmListe : liste) {
+                        for (film filmListe : listeFilm) {
                             filmselect = filmListe.FilmToString();
                             System.out.println(filmselect);
                         }
                         System.out.println("\nAppuyez sur une touche pour continuer");
                         System.in.read();
                         break;
+                    
+                        
+                    case 7 : //ajouter client
+                        System.out.println("Saisir nom client : ");
+                        nomClient = scanner.nextLine();
+                        
+                        System.out.println("Saisir prenom client : ");
+                        prenomClient = scanner.nextLine();
+                        
+                        System.out.println("Saisir mail client : ");
+                        mailClient = scanner.nextLine();
+                        
+                        listeClient.add(new client(nomClient, prenomClient, mailClient));
+                        break;
+                        
+                     case 8 : //afficher tous les clients
+                        for (client clientListe : listeClient) {
+                            clientSelect = clientListe.ClientInfo();
+                            System.out.println(clientSelect);
+                        }
+                        System.out.println("\nAppuyez sur une touche pour continuer");
+                        System.in.read();
+                        break;
+                        
+                        
+                    case 9 : //ajouter film à un client
+                        clientExist = false;
+                        do {
+                            System.out.println("Saisir nom client : ");
+                            nomClient = scanner.nextLine();
+                            //vérification si le client existe
+                            for (client clientListe : listeClient) {
+                                if (clientListe.GetNom().equals(nomClient)) {
+                                    clientExist = true;
+                                }
+                            }
+                        } while (clientExist == false);
+                        
+                        System.out.println("Saisir nom film : ");
+                        nomFilm = scanner.nextLine();
+                        
+                        for (client clientListe : listeClient) {
+                            if (clientListe.GetNom().equals(nomClient)) {
+                                clientListe.AddFilm(nomFilm);
+                            }
+                        }
+                        break;
+                        
+                    case 10 : //Afficher film client
+                        System.out.println("Saisir nom client : ");
+                        nomClient = scanner.nextLine();
+                        
+                        System.out.println("Liste des films de " + nomClient);
+                        for (client clientListe : listeClient) {
+                            if (clientListe.GetNom().equals(nomClient)) {
+                                System.out.println(clientListe.AfficherFilm());
+                            }
+                        }
+                        System.out.println("\nAppuyez sur une touche pour continuer");
+                        System.in.read();
+                        break;
+                        
                     default:
                         System.out.println("Erreur de saisie");                        
                         break;
--- MiniProjetJava/src/miniprojetjava/client.java
+++ MiniProjetJava/src/miniprojetjava/client.java
@@ -1,63 +1,100 @@
 package miniprojetjava;
 
+import java.lang.*;
+import java.util.*;
+import java.io.*;
+
 public class client
 {
     // variables d'instance - remplacez l'exemple qui suit par le vôtre
     public String nom;
     public String prenom;
     public String adressemail;
-    public int maxL;
-    
+    String films[] = {"", "", "" , "", ""};
+    public int i;
+    String listeFilmClient = "";
+
     public client(String nom, String prenom, String adressemail)
     {
         this.nom = nom;
         this.prenom = prenom;
         this.adressemail = adressemail;
-        this.maxL = 5;
+
     }
-    
+
     public String GetNom()
     {
         return nom;
     }
-    
+
     public String GetPrenom()
     {
         return prenom;
     }
-    
+
     public String GetAdrM()
     {
         return adressemail;
     }
-    
+
     public void SetNom(String nom)
     {
         this.nom = nom;
     }
-    
+
     public void SetPrenom(String prenom)
     {
         this.prenom = prenom;
     }
-    
+
     public void SetAdrM(String adressemail)
     {
         this.adressemail = adressemail;
     }
-    
-    public void SetMaxL(int maxL)
+
+    public String ClientHello()
     {
-        this.maxL=maxL;
+        return ("Bonjour " + prenom);
+    }
+
+    public String ClientInfo()
+    {
+        return ("Nom du client : " + nom + "\n" + "Prenom du client : " + prenom + "\n" + "Adresse Mail : " + adressemail + "\n");
+    }
+
+    public void AddFilm(String nomFilm)
+    {
+        for(i = 0; i<5; i++)
+        {
+            if(films[i].equals(""))
+            {
+                films[i]=nomFilm;
+                break;
+            }
+        }
+    }
+
+    public void DeleteFilm(String nomFilm)
+    {
+        for(i = 0; i<5; i++)
+        {
+            if(films[i].equals(nomFilm))
+            {
+                films[i]="";
+                break;
+            }
+        }
     }
     
-    public void ClientHello()
-    {
-        System.out.println("Bonjour " + prenom);
-    }
-    
-    public void ClientInfo()
-    {
-        System.out.println("Nom du client : " + nom + "\n" + "Prenom du client : " + prenom + "\n" + "Adresse Mail : " + adressemail + "\n");
+    public String AfficherFilm() {
+        for(i = 0; i<4; i++)
+        {
+            if(!films[i].equals(""))
+            {
+                listeFilmClient = listeFilmClient + films[i];
+                break;
+            }
+        }
+        return listeFilmClient;
     }
 }
