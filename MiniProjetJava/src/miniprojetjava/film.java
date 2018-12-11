f2e9b323fb4dda0c55415ea543fc7d0fea1dd034
Etienne Fonteix <etienne.fonteix@gmail.com>
2018-12-11 08:33:27 +0100

    Ajout class film

---
+++ MiniProjetJava/src/miniprojetjava/film.java
@@ -0,0 +1,21 @@
+package miniprojetjava;
+
+public class film {
+    
+    public String nom;
+    public boolean dispo;
+    public String acteur;
+    public int annee;
+    public String realisateur;
+    public String support;
+    
+    public film (String nom, boolean dispo, String acteur, int annee, String realisateur, String support) {
+        this.nom = nom;
+        this.dispo = dispo;
+        this.acteur = acteur;
+        this.annee = annee;
+        this.realisateur = realisateur;
+        this.support = support;
+        
+    }
+}
